3a6cbd35d43d3478e92a8b34b6710988095ba2ff
Scott Walker <scott@swlt.ru>
2025-08-15 16:52:30 +0300

    api. обновление пакетов + env.example

--- api/drizzle.config.js
+++ api/drizzle.config.js
@@ -1,12 +1,14 @@
 import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 
+console.log(process.env.DATABASE_URL)
+
 export default defineConfig({
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL
+  },
   schema: "./src/db/schema",
   out: "./runtime/migrations",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL
-  },
   verbose: true
 })
