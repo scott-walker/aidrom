c7f801d5d4d75bc641517891d45242ea0d0f14f7
Scott Walker <scott@swlt.ru>
2025-08-18 11:21:26 +0300

    front. коммент для лейаута

--- front/src/layouts/main.tsx
+++ front/src/layouts/main.tsx
@@ -1,9 +1,14 @@
-import type { PropsWithChildren } from "react"
-import Sidebar from "@/components/Sidebar"
-import Header from "@/components/Header"
-import Content from "@/components/Content"
+import type { PropsWithChildren, JSX } from "react"
+import Sidebar from "@components/Sidebar"
+import Header from "@components/Header"
+import Content from "@components/Content"
 
-export default function MainLayout({ children }: PropsWithChildren) {
+/**
+ * Основной компонент макета
+ * @param {PropsWithChildren} children - Дочерние компоненты
+ * @returns {JSX.Element} - Основной компонент макета
+ */
+export default function MainLayout({ children }: PropsWithChildren): JSX.Element {
   return (
     <div className="h-screen flex">
       <Sidebar />
