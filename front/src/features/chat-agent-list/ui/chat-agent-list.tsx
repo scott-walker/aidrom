313a11b521e58cf31dd294223b5d9d608d2c022c
Scott Walker <scott@swlt.ru>
2025-09-19 22:32:00 +0300

    front. фикс списка чатов

--- front/src/features/chat-agent-list/ui/chat-agent-list.tsx
+++ front/src/features/chat-agent-list/ui/chat-agent-list.tsx
@@ -10,7 +10,7 @@ import { useAgentChats } from "../lib/use-agent-chats"
  */
 export const ChatAgentList = () => {
   const { agentChats, isLoading, error } = useAgentChats()
-  const containerClasses = makeClasses("flex", "flex-col", "gap-3", "px-(--layout-inner-offset-x)", "py-2", "h-full")
+  const containerClasses = makeClasses("flex", "flex-col", "gap-3", "px-(--layout-inner-offset-x)", "pt-2", "pb-8")
 
   if (isLoading) {
     return (
