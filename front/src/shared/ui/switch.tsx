d1e4b4038a5b7d8598d4e4dd08c473ec197b2525
Scott Walker <scott@swlt.ru>
2025-09-22 21:17:47 +0300

    front. Добавление компонента переключателя с использованием @radix-ui/react-switch, обновление зависимостей в package.json и package-lock.json.

---
+++ front/src/shared/ui/switch.tsx
@@ -0,0 +1,63 @@
+import * as RadixSwitch from "@radix-ui/react-switch"
+import { makeClasses } from "@lib/style-api"
+
+/**
+ * Пропсы
+ * @namespace Shared.UI.Switch.Props
+ */
+export interface SwitchProps {
+  disabled?: boolean
+  checked?: boolean
+  className?: string
+  onChange?: (checked: boolean) => void
+}
+
+/**
+ * Переключатель
+ * @namespace Shared.UI.Switch
+ */
+export const Switch = ({ disabled, checked, onChange, className }: SwitchProps) => {
+  const rootClasses = makeClasses(
+    "flex",
+    "items-center",
+    "justify-start",
+    // "data-[state=checked]:justify-end",
+    "w-10",
+    "h-6",
+    "rounded-full",
+    "bg-background-hard",
+    "ring-2",
+    "ring-transparent",
+    "ring-offset-3",
+    "ring-offset-background-soft",
+    "transition-color",
+    "duration-200",
+    "ease-in-out",
+    "hover:ring-foreground-soft",
+    "data-[state=checked]:hover:ring-primary",
+    "cursor-pointer",
+    "select-none",
+    className
+  )
+  const thumbClasses = makeClasses(
+    "w-6",
+    "h-6",
+    "rounded-full",
+    "bg-foreground-soft",
+    "transition-transform",
+    "duration-200",
+    "ease-in-out",
+    "data-[state=checked]:bg-primary",
+    "data-[state=checked]:translate-x-4"
+  )
+
+  const handleChange = (checked: boolean) => {
+    onChange?.(checked)
+  }
+
+  return (
+    <RadixSwitch.Root disabled={disabled} checked={checked} onCheckedChange={handleChange} className={rootClasses}>
+      <RadixSwitch.Thumb className={thumbClasses} />
+    </RadixSwitch.Root>
+  )
+}
