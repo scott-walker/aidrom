2cae1bff9cca137b08948dee999eda9542d14a2f
Scott Walker <scott@swlt.ru>
2025-10-02 15:47:39 +0300

    front. Обновлены конфигурации для фронтенда: добавлен новый сервис front-build с настройками для сборки, изменены переменные окружения и пути к файлам конфигурации nginx. Упрощен процесс запуска и настройки приложения.

--- front/docker/build/entrypoint.sh
+++ front/docker/build/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
-export PORT=${PORT:-80}
+export NGINX_PORT=${NGINX_PORT:-80}
 
-echo "Запуск nginx на порту ${PORT}..."
+envsubst '
+  $NGINX_PORT
+' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "Запуск nginx на порту ${NGINX_PORT}..."
 exec nginx -g "daemon off;"
